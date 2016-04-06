248339ef24baace4a5f798e9aa20fd147594f392
thatgirlcancode16 <thatgirlcancode@gmail.com>
2016-04-06 10:17:20 -0600

    Another update

--- test_enigma.rb
+++ test_enigma.rb
@@ -24,32 +24,66 @@ class TestEnigma < Minitest::Test
 
   end
 
-def test_offset_calculator
-  rotation = [84, 46, 68, 85]
-  offsets = [3, 0, 5, 6]
-  offsets_result = [87, 46, 73, 91]
-  assert_equal Enigma.offset_calculator(rotation,offsets) , offsets_result
-end
+  def test_offset_calculator
+    enigma = Enigma.new
+    rotation = [84, 46, 68, 85]
+    offsets = [3, 0, 5, 6]
+    offsets_result = [87, 46, 73, 91]
+    assert_equal enigma.offset_calculator(rotation,offsets) , offsets_result
+  end
 
-# def test_self.split_message_into_chunks
-# end
-#
-# def chunk_rotate
-# end
-#
-# def rotate_chunk
-# end
-#
-# def encyrpt
-# end
-#
-# def decrypt
-# end
-#
-# def rotate_char
-# end
-#
-# def
-# end
+  def test_self_split_message_into_chunks
+    message = "pizza"
+    assert_equal [["p", "i", "z", "z"], ["a"]], Enigma.split_message_into_chunks(message)
+  end
+  #
+  # def chunk_rotate
+  # end
+  #
+  # def rotate_chunk
+  # end
+  #
+  def test_encyrpt
+    message_to_encrypt = 'pizza'
+    message_encrypted = 'sp06d'
+    key = '00740'
+    date = '060416'
+
+    #instantiate classes
+    enigma_class_variable = Enigma.new(message_to_encrypt, Enigma.get_character_map)
+
+    #get rotations and date offsets
+    rotations = Enigma.key_rotation(key)
+    offset_for_date = Enigma.offset_for_date(date)
+
+    #use rotations and offsets for date to get offsets
+    enigma_class_variable.offset_calculator(rotations, offset_for_date)
+
+    #encrypt and store in variable to be used in assert_equal
+    encrypted_message = enigma_class_variable.encrypt
+
+    assert_equal message_encrypted, encrypted_message
+  end
+
+  def test_decrypt
+    encrypted_message = 'wedlh'
+    key = '43519'
+    date = '060416'
+    decrypted = 'pizza'
+    enigma_class_variable = Enigma.new(encrypted_message, Enigma.get_character_map)
+
+    rotations = Enigma.key_rotation(key)
+    offset_for_date = Enigma.offset_for_date(date)
+    enigma_class_variable.offset_calculator(rotations, offset_for_date)
+
+    decrypted_message = enigma_class_variable.decrypt
+    assert_equal decrypted, enigma_class_variable.decrypt
+  end
+  #
+  # def rotate_char
+  # end
+  #
+  # def
+  # end
 
 end
